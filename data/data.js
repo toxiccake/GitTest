d72cd52aa7e771f75f1e5ec4ea32411014e95834
TOXiC.Cake <70811330+toxiccake@users.noreply.github.com>
2023-03-08 13:52:33 -0500

    Ajout DATA

---
+++ data/data.js
@@ -0,0 +1,43 @@
+let hospList = 
+[
+	{"buildCode":1234, "fileNo":5, "admDate":"14-août-00", "outDate":"14-août-01", "spec":"medecine"},
+	{"buildCode":1234, "fileNo":10, "admDate":"12-déc-92", "outDate":"12-déc-92", "spec":"chirurgie"},
+	{"buildCode":2346, "fileNo":8, "admDate":"03-mars-03", "outDate":"05-mars-03", "spec":"medecine"},
+	{"buildCode":2346, "fileNo":1, "admDate":"11-Nov-97", "outDate":"12-Nov-97", "spec":"orthopédie"},
+	{"buildCode":2346, "fileNo":3, "admDate":"12-avr-95", "outDate":"30-avr-95", "spec":"medecine"},
+	{"buildCode":3980, "fileNo":5, "admDate":"14-févr-00", "outDate":"14-mars-00", "spec":"medecine"},
+	{"buildCode":3980, "fileNo":5, "admDate":"01-janv-01", "outDate":"01-févr-01", "spec":"medecine"},
+	{"buildCode":3980, "fileNo":9, "admDate":"03-avr-95", "outDate":"08-avr-95", "spec":"orthopédie"},
+	{"buildCode":3980, "fileNo":7, "admDate":"27-Nov-99", "outDate":"04-déc-99", "spec":"chirurgie"},
+	{"buildCode":3980, "fileNo":10, "admDate":"28-avr-97", "outDate":"05-mai-97", "spec":"chirurgie"},
+	{"buildCode":4177, "fileNo":3, "admDate":"3-août-01", "outDate":"05-déc-01", "spec":"medecine"},
+	{"buildCode":4177, "fileNo":3, "admDate":"02-févr-02", "outDate":"23-févr-02", "spec":"orthopédie"},
+	{"buildCode":7306, "fileNo":2, "admDate":"23-mai-98", "outDate":"27-mai-98", "spec":"orthopédie"},
+	{"buildCode":8895, "fileNo":10, "admDate":"23-août-98", "outDate":"27-août-98", "spec":"chirurgie"},
+	{"buildCode":7306, "fileNo":2, "admDate":"23-févr-98", "outDate":"27-févr-98", "spec":"orthopédie"}
+]
+
+let buildList = 
+[
+	{"buildCode":1234, "name":"Centre hospitalier de St. Mary", "addr":"3830, avenue Lacombe Montréal, Qc", "postCode":"H3T 1M5", "phone":"(514)-345-3511"},
+	{"buildCode":2346, "name":"Hôpital Saint-Luc du CHUM", "addr":"1058, rue Saint-Denis, Montréal, Qc", "postCode":"H2X 3J4", "phone":"(514)-890-8000"},
+	{"buildCode":3980, "name":"Hôpital Santa Cabrini", "addr":"5655, rue Saint-Zotique Est Montréal,Qc", "postCode":"H1T 1P7", "phone":"(514)-252-6000"},
+	{"buildCode":4177, "name":"Hôpital Jean-Talon", "addr":"1385, rue Jean-Talon Est Montréal, Qc", "postCode":"H2E 1S6", "phone":"(514)-495-6767"},
+	{"buildCode":7306, "name":"Hôpital Fleury", "addr":"2180, rue Fleury Est Montréal, Qc", "postCode":"H2B 1K3", "phone":"(514)-384-2000"},
+	{"buildCode":8895, "name":"Hôpital Rivière-des-Prairies", "addr":"7070, boulevard Perras Montréal, Qc", "postCode":"H1E 1A4", "phone":"(514)-323-7260"},
+	{"buildCode":3655, "name":"Hôpital St-Hubert", "addr":"7070, boulevard Perras Montréal, Qc", "postCode":"H1E 1A4", "phone":"(514)-323-7260"}
+]
+
+let patientList = 
+[
+	{"fileNo":1, "lName":"Léger", "fName":"Émile", "dob":"26mars1917", "gender":"M"},
+	{"fileNo":2, "lName":"Bernard", "fName":"Marie", "dob":"3février1946", "gender":"F"},
+	{"fileNo":3, "lName":"Chartrand", "fName":"Guy", "dob":"5avril1959", "gender":"M"},
+	{"fileNo":4, "lName":"Côté", "fName":"André", "dob":"2septembre1978", "gender":"M"},
+	{"fileNo":5, "lName":"Lavoie", "fName":"Carole", "dob":"4novembre1973", "gender":"F"},
+	{"fileNo":6, "lName":"Martin", "fName":"Diane", "dob":"2décembre1965", "gender":"F"},
+	{"fileNo":7, "lName":"Lacroix", "fName":"Pauline", "dob":"25janvier1956", "gender":"F"},
+	{"fileNo":8, "lName":"St-Jean", "fName":"Arther", "dob":"7octobre1912", "gender":"M"},
+	{"fileNo":9, "lName":"Béchard", "fName":"Marc", "dob":"8août1980", "gender":"M"},
+	{"fileNo":10, "lName":"Chartrand", "fName":"Mario", "dob":"23juillet1947", "gender":"M"}
+]
